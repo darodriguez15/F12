0611b5da2a126b5d6c57188a6fcfc67ea1c53d52
Diego Andrés Rodriguez <da.rodriguez15@uniandes.edu.co>
2017-09-21 10:29:52 -0500

    Arreglo forzado

--- app.js
+++ app.js
@@ -6,17 +6,13 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var drivers = require('./routes/drivers');
-var stats = require('./routes/stats');
-var teams = require('./routes/teams');
-var circuits = require('./routes/circuits');
-var fan = require('./routes/fanZone');
+var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,24 +20,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'front/build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-
-
-app.use('/driversinfo', drivers);
-app.use('/statsinfo', stats);
-app.use('/teamsinfo', teams);
-app.use('/circuitInfo', circuits);
-
-app.use('/fans', fan);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
-  //next(err);
-  res.sendFile(path.resolve(__dirname, '.', './front/build', 'index.html'));
+  next(err);
 });
 
 // error handler
